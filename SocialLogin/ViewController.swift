b8f45f20c9e88d229e092e562e0b639413197ec4
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-09-22 14:16:01 +0530

    Initial Commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by MACBOOK on 22/09/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
