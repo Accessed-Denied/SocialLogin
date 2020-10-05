2dd6279b5b16bd775c07b79784ffdaf2748d9fbc
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-10-06 00:11:56 +0530

    code modified

--- SocialLogin/View/InstagramVC.swift
+++ SocialLogin/View/InstagramVC.swift
@@ -35,21 +35,9 @@ extension InstagramVC: WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         //Get user id and token
         let request = navigationAction.request
-        print(navigationAction.request)
-//        if let authorizedCode = self.instagramApi.getTokenFromCallbackURL(request: request){
-//            print(authorizedCode)
-//        }
-//        if let url = navigationAction.request.url {
-//        if url.absoluteString.contains("oauth/access_token") {
-//            self.instagramApi.postApiData(requestUrl: request, resultType: <#T##Decodable.Protocol#>, completionHandler: <#T##(Decodable) -> Void#>)(request: request) { (instagramTestUser) in
-//                print(instagramTestUser)
-//                    self.testUserData = instagramTestUser
-            //        self.presentAuth = false
-//                    }
-//            }
-//        }
-        self.instagramApi.postApiData(requestUrl: request, resultType: InstagramTestUser.self) { (response) in
-            print(response)
+        if let authorizedCode = self.instagramApi.getTokenFromCallbackURL(request: request){
+            print(authorizedCode)
+            socialVM.getInstagramToken(request: InstagramTokenRequest(code: authorizedCode))
         }
         decisionHandler(.allow)
     }
